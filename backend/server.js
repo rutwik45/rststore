dd87fd102bee3da4c86ca7468a9c55de33661e2e
unknown <rutwikbhatde@gmail.com>
2022-07-10 08:28:16 +0530

    ready for developement

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,7 @@ console.log(__dirname)
 app.use('/uploads',express.static(path.join(__dirname, '/uploads')))
 
 if (process.env.NODE_ENV==='production') {
-    app.use(express.static(path.join(__dirname,'/frontend/build')))
+    app.use(express.static(path.join(__dirname, '/frontend/build')))
     app.get('*',(req,res)=>{
         res.sendFile(path.resolve(__dirname,'frontend','build','index.html'))
     })
